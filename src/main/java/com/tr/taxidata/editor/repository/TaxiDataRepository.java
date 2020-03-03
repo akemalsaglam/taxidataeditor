dad79751cc2066c0d4806fb578eb8b8db3a3ae60
akemalsaglam <alikemalsaglam90@gmail.com>
2020-03-03 08:52:19 +0300

    refactor.

--- src/main/java/com/tr/taxidata/editor/repository/TaxiDataRepository.java
+++ src/main/java/com/tr/taxidata/editor/repository/TaxiDataRepository.java
@@ -16,37 +16,37 @@ public interface TaxiDataRepository extends JpaRepository<TaxiData, Long> {
     @Query(value = "select * from public.bursa_taxi_data_month1 tdmv where tdmv.taxi_id=?1 and tdmv.date>'2019-01-01 00:00:00' and tdmv.date<'2019-01-02 00:00:00' order by \"date\" asc", nativeQuery = true)
     List<TaxiData> getMonth1DataByTaxiId(Long id);
 
-    @Query(value = "select * from public.taxi_data_month2_view tdmv where tdmv.taxi_id=?1", nativeQuery = true)
+    @Query(value = "select * from public.bursa_taxi_data_month2 tdmv where tdmv.taxi_id=?1 and tdmv.date>'2019-01-01 00:00:00' and tdmv.date<'2019-01-02 00:00:00' order by \"date\" asc", nativeQuery = true)
     List<TaxiData> getMonth2DataByTaxiId(Long id);
 
-    @Query(value = "select * from public.taxi_data_month3_view tdmv where tdmv.taxi_id=?1", nativeQuery = true)
+    @Query(value = "select * from public.bursa_taxi_data_month3 tdmv where tdmv.taxi_id=?1 and tdmv.date>'2019-01-01 00:00:00' and tdmv.date<'2019-01-02 00:00:00' order by \"date\" asc", nativeQuery = true)
     List<TaxiData> getMonth3DataByTaxiId(Long id);
 
-    @Query(value = "select * from public.taxi_data_month4_view tdmv where tdmv.taxi_id=?1", nativeQuery = true)
+    @Query(value = "select * from public.bursa_taxi_data_month4 tdmv where tdmv.taxi_id=?1 and tdmv.date>'2019-01-01 00:00:00' and tdmv.date<'2019-01-02 00:00:00' order by \"date\" asc", nativeQuery = true)
     List<TaxiData> getMonth4DataByTaxiId(Long id);
 
-    @Query(value = "select * from public.taxi_data_month5_view tdmv where tdmv.taxi_id=?1", nativeQuery = true)
+    @Query(value = "select * from public.bursa_taxi_data_month5 tdmv where tdmv.taxi_id=?1 and tdmv.date>'2019-01-01 00:00:00' and tdmv.date<'2019-01-02 00:00:00' order by \"date\" asc", nativeQuery = true)
     List<TaxiData> getMonth5DataByTaxiId(Long id);
 
-    @Query(value = "select * from public.taxi_data_month6_view tdmv where tdmv.taxi_id=?1", nativeQuery = true)
+    @Query(value = "select * from public.bursa_taxi_data_month6 tdmv where tdmv.taxi_id=?1 and tdmv.date>'2019-01-01 00:00:00' and tdmv.date<'2019-01-02 00:00:00' order by \"date\" asc", nativeQuery = true)
     List<TaxiData> getMonth6DataByTaxiId(Long id);
 
-    @Query(value = "select * from public.taxi_data_month7_view tdmv where tdmv.taxi_id=?1", nativeQuery = true)
+    @Query(value = "select * from public.bursa_taxi_data_month7 tdmv where tdmv.taxi_id=?1 and tdmv.date>'2019-01-01 00:00:00' and tdmv.date<'2019-01-02 00:00:00' order by \"date\" asc", nativeQuery = true)
     List<TaxiData> getMonth7DataByTaxiId(Long id);
 
-    @Query(value = "select * from public.taxi_data_month8_view tdmv where tdmv.taxi_id=?1", nativeQuery = true)
+    @Query(value = "select * from public.bursa_taxi_data_month8 tdmv where tdmv.taxi_id=?1 and tdmv.date>'2019-01-01 00:00:00' and tdmv.date<'2019-01-02 00:00:00' order by \"date\" asc", nativeQuery = true)
     List<TaxiData> getMonth8DataByTaxiId(Long id);
 
-    @Query(value = "select * from public.taxi_data_month9_view tdmv where tdmv.taxi_id=?1", nativeQuery = true)
+    @Query(value = "select * from public.bursa_taxi_data_month9 tdmv where tdmv.taxi_id=?1 and tdmv.date>'2019-01-01 00:00:00' and tdmv.date<'2019-01-02 00:00:00' order by \"date\" asc", nativeQuery = true)
     List<TaxiData> getMonth9DataByTaxiId(Long id);
 
-    @Query(value = "select * from public.taxi_data_month10_view tdmv where tdmv.taxi_id=?1", nativeQuery = true)
+    @Query(value = "select * from public.bursa_taxi_data_month10 tdmv where tdmv.taxi_id=?1 and tdmv.date>'2019-01-01 00:00:00' and tdmv.date<'2019-01-02 00:00:00' order by \"date\" asc", nativeQuery = true)
     List<TaxiData> getMonth10DataByTaxiId(Long id);
 
-    @Query(value = "select * from public.taxi_data_month11_view tdmv where tdmv.taxi_id=?1", nativeQuery = true)
+    @Query(value = "select * from public.bursa_taxi_data_month11 tdmv where tdmv.taxi_id=?1 and tdmv.date>'2019-01-01 00:00:00' and tdmv.date<'2019-01-02 00:00:00' order by \"date\" asc", nativeQuery = true)
     List<TaxiData> getMonth11DataByTaxiId(Long id);
 
-    @Query(value = "select * from public.taxi_data_month12_view tdmv where tdmv.taxi_id=?1", nativeQuery = true)
+    @Query(value = "select * from public.bursa_taxi_data_month12 tdmv where tdmv.taxi_id=?1 and tdmv.date>'2019-01-01 00:00:00' and tdmv.date<'2019-01-02 00:00:00' order by \"date\" asc", nativeQuery = true)
     List<TaxiData> getMonth12DataByTaxiId(Long id);
 
     @Query(value = "select btd.taxi_id as taxiId,count(*) as logCount from public.bursa_taxi_data_month1 btd group by btd.taxi_id order by logCount desc limit ?1", nativeQuery = true)
